b62a212f73aa0275091ec6190b972e27903b6db2
mark161r <mkghome@gmail.com>
2020-06-25 14:21:14 +0300

    Пятое задание

--- HomeWorksPython/Lesson_1/L_5.py
+++ HomeWorksPython/Lesson_1/L_5.py
@@ -27,7 +27,7 @@ if u_vir > u_izd:
     dahod_sotr = fin_p / kol_sotr
     print('\t', 'Фирма работает с прибылью, она состовляет = {}\n'.format(fin_p),
           '\t', 'Соотношение = {:>.1f}\n'.format(fin_r),
-          '\t', 'Прибыль на одного сострудника равна = {:>.0f}'.format(dahod_sotr))
+          '\t', 'Прибыль на одного сострудника равна = {}'.format(int(dahod_sotr)))
 elif u_vir == u_izd:
     print('Фирма работает без убытков и даходов.')
 else:
