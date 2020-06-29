aced66a0429a4dd2828762c55d2bda54a87e3149
mark161r <mkghome@gmail.com>
2020-06-29 17:58:56 +0300

    Второе задание

--- HomeWorksPython/Lesson_2/L_2.py
+++ HomeWorksPython/Lesson_2/L_2.py
@@ -1,11 +1,11 @@
-user_s = input('Введите список через запятую:\n')
+user_s = input('Введите значения списка через пробел:\n')
 print(user_s)
-user_list = user_s.split(',')
-print(user_list)
+user_list = user_s.split(' ')
 
 s = 0 #счетчик
 
 while s < len(user_list[:-1]):
     user_list[s], user_list[s + 1] = user_list[s + 1], user_list[s]
     s += 2
-    print(user_list)
+
+print(user_list)
