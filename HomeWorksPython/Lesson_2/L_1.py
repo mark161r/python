30d17d7b8e6f879449ac03c86bddfeb054a9e9d4
mark161r <mkghome@gmail.com>
2020-06-29 18:04:23 +0300

    Первое задание

--- HomeWorksPython/Lesson_2/L_1.py
+++ HomeWorksPython/Lesson_2/L_1.py
@@ -5,4 +5,4 @@ list1 = [25, -123, None, 'Srt', True, 3.14, [1, 2]]
 print(list1,'\n')
 print('Соответствие типов:')
 for i in list1:
-    print('{} является типом {}'.format(i, type(list1)))
+    print('{} является типом {}'.format(i, type(i)))
