cfc642431085f5b64b201d9bc21576bb9f949a79
mark161r <mkghome@gmail.com>
2020-06-30 18:54:27 +0300

    Четвертое задание

--- HomeWorksPython/Lesson_2/L_4.py
+++ HomeWorksPython/Lesson_2/L_4.py
@@ -2,10 +2,12 @@ print('Вывод каждого слова с новой строки для в
 
 user_str = input("Введите предложение:\n")
 idx = user_str.split(' ')
+empty = ''
 for i, el in enumerate(idx, 1):
-    if len(el) > 10:
+    if user_str == empty:
+        empty = None
+        print('Вы ничего не ввели!')
+    else:
+        len(el) > 10
         el = el[0:10]
         print('{}. - {}'.format(i, el))
-    else:
-        len(el) == 0
-        print('Вы ничего не ввели!')
