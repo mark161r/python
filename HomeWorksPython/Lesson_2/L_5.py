a18cc3cd94f63f78e9a2315781ff6f4cf0a6da19
mark161r <mkghome@gmail.com>
2020-07-09 16:40:00 +0300

    Пятое задание

---
+++ HomeWorksPython/Lesson_2/L_5.py
@@ -0,0 +1,20 @@
+user_list = [7, 5, 3, 3, 2]
+print("Необходимо вводить натуральные числа по одному.\n"
+      "Для завершения работы введите exit.\n")
+
+while True:
+    user_element = input("Введите натуральное число - ")
+    if user_element.isdigit():
+        user_element = int(user_element)
+        break
+    print('Ввдено не число! Попробуйте еще раз.')
+
+if user_element in user_list:
+    user_list.insert(user_list.index(user_element), user_element)
+elif not user_element:
+    print(user_list)
+elif user_list and user_element > max(user_list):
+        user_list.insert(0, user_element)
+else:
+    user_list.append(user_element)
+print(user_list)
